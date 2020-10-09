97d051e2faa77864710c2518f539a0b088d0ed70
Bipin Bohara <bipinbohara90@gmail.com>
2020-10-09 18:31:23 +0545

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import json
 import logging
 from Packages import *
-#import requests
 import urllib3
 
 logger = logging.getLogger()
@@ -14,11 +13,8 @@ def lambda_handler1(event, context, city_name):
     api_key = '9fdb700b5a2efd806c8ffd6f356e559f'
     base_url = 'http://api.openweathermap.org/data/2.5/weather?'
     
-    #city_name = "London"
-    
     finalurl = base_url + 'appid=' + api_key + '&q=' + city_name
     
-    #response = requests.get(finalurl)
     r = http.request('GET', finalurl)
     x = json.loads(r.data.decode('utf-8'))
     y = x['main']
@@ -30,5 +26,4 @@ def lambda_handler1(event, context, city_name):
     
     content = "Temperature (in Farenheit) = " + str("%.2f" % current_temperature) + "\nAtmospheric pressure (in hPa) = " + str(current_pressure) + "\nHumidity (in percentage) = " + str(current_humidiy) + "\nDescription = " + str(weather_description)
     
-    #content = "You are trapped in test."
     return (content)
